41b422a4824de59df8aeea53556ca28bcf7450fd
raghavnimbalkar1 <raghav.nimbalkar@gmail.com>
2025-03-11 00:09:48 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-blue-500 text-white p-4 text-center">
+      Tailwind CSS and TypeScript with App Router are Working! 🚀
+    </div>
+  );
+}
